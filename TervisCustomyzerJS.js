6920e1205b436a6cc6ac1f252e99473dc455230b
Chris Magnuson <hourglasssand@gmail.com>
2019-08-06 15:15:15 -0400

    Move functions to new module

--- TervisCustomyzerJS.js
+++ TervisCustomyzerJS.js
@@ -1,99 +1,9 @@
-import { Add_MemberScriptProperty, New_HashTableIndex, ConvertFrom_StringUsingRegexCaptureGroup } from '@tervis/tervisutilityjs/TervisUtilityJS.js'
-import { Get_SizeAndFormTypeMetaData, Get_ColorCodeToMarketingNameMapping } from '@tervis/tervisproductsizeandformtypemetadata'
 import {
     New_TervisAdobeScene7CustomyzerArtboardImageURL,
     New_TervisAdobeScene7URL,
     New_TervisAdobeScene7WrapDecoration3TimesURL,
     New_TervisAdobeScene7ProductVirtualURL
 } from '@tervis/tervisadobescene7js'
-
-export async function Get_CustomyzerPrintImageTemplateSizeAndFormType ({
-    $PrintImageTemplateName
-}) {
-    var $PrintImageTemplateNameToSizeAndFormTypeIndex = await Get_PrintImageTemplateNameToSizeAndFormTypeIndex()
-    let {Size, FormType} = $PrintImageTemplateNameToSizeAndFormTypeIndex[$PrintImageTemplateName]
-    
-    if (Array.isArray(FormType)) {
-        var $FormType = FormType[0]
-    } else {
-        var $FormType = FormType
-    }
-    
-    return {$Size: Size, $FormType}
-}
-
-export async function Get_CustomyzerImageTemplateName ({
-    $Size,
-    $FormType,
-    $TemplateType,
-    $PrintTemplateType
-}) {
-    var $SizeAndFormTypeToImageTemplateNamesIndex = await Get_SizeAndFormTypeToImageTemplateNamesIndex()
-    let $Template = $SizeAndFormTypeToImageTemplateNamesIndex[`${$Size}${$FormType}`].ImageTemplateName[$TemplateType]
-    if (!$PrintTemplateType) {
-        return $Template
-    } else {
-        return $Template[$PrintTemplateType]
-    }
-}
-
-
-export async function Get_SizeAndFormTypeMetaDataUsingIndex ({
-    $Size,
-    $FormType
-}) {
-    var $SizeAndFormTypeToImageTemplateNamesIndex = await Get_SizeAndFormTypeToImageTemplateNamesIndex()
-    return $SizeAndFormTypeToImageTemplateNamesIndex[`${$Size}${$FormType}`]
-}
-
-var $SizeAndFormTypeMetaData
-async function Get_SizeAndFormTypeMetaDataWithCustomProperties () {
-    if (!$SizeAndFormTypeMetaData) {
-        $SizeAndFormTypeMetaData = await Get_SizeAndFormTypeMetaData()
-    }
-    return $SizeAndFormTypeMetaData
-}
-
-var $SizeAndFormTypeToImageTemplateNamesIndex
-async function Get_SizeAndFormTypeToImageTemplateNamesIndex () {
-    if (!$SizeAndFormTypeToImageTemplateNamesIndex) {
-        var $SizeAndFormTypeMetaData = await Get_SizeAndFormTypeMetaDataWithCustomProperties()
-        $SizeAndFormTypeToImageTemplateNamesIndex = New_HashTableIndex({$InputObject: $SizeAndFormTypeMetaData, $PropertyToIndex: "SizeAndFormTypes"})
-    }
-    
-    return $SizeAndFormTypeToImageTemplateNamesIndex
-}
-
-var $PrintImageTemplateNameToSizeAndFormTypeIndex
-async function Get_PrintImageTemplateNameToSizeAndFormTypeIndex () {
-    if (!$PrintImageTemplateNameToSizeAndFormTypeIndex) {
-        var $SizeAndFormTypeMetaData = await Get_SizeAndFormTypeMetaDataWithCustomProperties()
-        $PrintImageTemplateNameToSizeAndFormTypeIndex = New_HashTableIndex({ $InputObject: $SizeAndFormTypeMetaData, $PropertyToIndex: "PrintImageTemplateNames"})
-    }
-    return $PrintImageTemplateNameToSizeAndFormTypeIndex
-}
-
-var $ColorCodeToMarketingNameIndex
-export async function Get_ColorCodeToMarketingNameIndex () {
-    if (!$ColorCodeToMarketingNameIndex) {
-        var $ColorCodeToMarketingNameMapping = await Get_ColorCodeToMarketingNameMapping()
-        $ColorCodeToMarketingNameIndex = New_HashTableIndex({ $InputObject: $ColorCodeToMarketingNameMapping, $PropertyToIndex: "ColorCode"})
-    }
-    return $ColorCodeToMarketingNameIndex
-}
-
-export function Get_SizeAndFormTypeFromString ({
-    $String
-  }) {
-    var $Results = ConvertFrom_StringUsingRegexCaptureGroup({
-      $Regex: /(?<$Size>\d*)(?<$FormType>\w*)/u,
-      $String
-    })
-
-    if ($Results) {
-      return $Results
-    }
-  }
 
 export async function New_CustomyzerProjectProductVirtualURL ({
     $ProjectID,
